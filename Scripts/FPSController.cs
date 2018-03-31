a971972d9f4b5054784c1fdf6561ec16c4e5db7b
zero3620 <36128598+zero3620@users.noreply.github.com>
2018-03-31 23:09:56 +0530

    Add FPSController.cs
    Added the base of any First Person Controller with rigidbody physics.

---
+++ Scripts/FPSController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class FPSController : MonoBehaviour {
+
+	public float move_speed = 10.0F;
+	public float turn_speed = 2.0F;
+	[Range(-1,1)]
+	public int y_invert = -1;
+	public bool isDebug = true;
+	public LayerMask object_layer;
+
+	private Camera mainCam;
+	private Rigidbody player;
+
+	void Start () {
+		
+		player = GetComponent<Rigidbody> ();
+		mainCam = Camera.main;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	
+	}
+		
+	void Update () {
+
+		if (isDebug) {
+
+			//Write devolopment build code here
+			if (Input.GetKey (KeyCode.Escape) && !Cursor.visible) {
+				Cursor.visible = true;
+				Cursor.lockState = CursorLockMode.None;
+			} else if (Input.GetMouseButtonDown (0)) {
+				if (Cursor.visible) {
+					Cursor.visible = false;
+					Cursor.lockState = CursorLockMode.Locked;
+				}
+			}
+
+		}
+
+		#region Movement
+		float rotX = Input.GetAxis ("Mouse Y");
+		float rotY = Input.GetAxis ("Mouse X");
+
+		float moveX = move_speed * Input.GetAxis ("Horizontal");
+		float moveY = move_speed * Input.GetAxis ("Vertical");
+
+		Vector3 velocity = new Vector3 (0, 0);
+		velocity = transform.forward * moveY + transform.right * moveX;
+
+		player.velocity = velocity;
+
+		player.transform.Rotate (Vector3.up * rotY, Space.World);
+		mainCam.transform.Rotate (Vector3.right * rotX * y_invert);
+		#endregion
+
+		#region Object Detection
+		
+		RaycastHit hit;
+		Ray ray = mainCam.ViewportPointToRay(new Vector2(0.5F,0.5F));
+		if(Physics.Raycast(ray, out hit, 100.0f, object_layer)){
+			GameObject hitObj = hit.collider.gameObject;
+			Debug.Log(hitObj.ToString());
+		}
+
+		#endregion
+	
+	}
+
+}
