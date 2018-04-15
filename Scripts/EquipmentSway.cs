80262b066329a12e55dfc1d48a13b3e4551ea96c
zero3620 <36128598+zero3620@users.noreply.github.com>
2018-04-15 18:17:03 +0530

    Add EquipmentSway and update FPSController

---
+++ Scripts/EquipmentSway.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+namespace Effects
+{
+    public class EquipmentSway : MonoBehaviour
+    {
+
+        public float sway_speed = 0.5f;
+        public float maxSway = 20.0f;
+
+        void Update()
+        {
+
+            float rotCamX = Input.GetAxis("Mouse X") * sway_speed;
+            float rotCamY = Input.GetAxis("Mouse Y") * sway_speed;
+            if (Mathf.Abs(rotCamX) > 20.0f)
+            {
+                rotCamX = 20.0f * Mathf.Sign(rotCamX);
+            }
+            if (Mathf.Abs(rotCamY) > 20.0f)
+            {
+                rotCamY = 20.0f * Mathf.Sign(rotCamY);
+            }
+            transform.Rotate(transform.up * rotCamX * -1);
+            transform.Rotate(transform.right * rotCamY * -1);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Camera.main.transform.rotation, sway_speed/2.0f);
+
+        }
+    }
+}
--- Scripts/FPSController.cs
+++ Scripts/FPSController.cs
@@ -1,74 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+namespace Controllers
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FPSController : MonoBehaviour
+    {
 
-[RequireComponent(typeof(Rigidbody))]
-public class FPSController : MonoBehaviour {
+        public float move_speed = 10.0F;
+        [Range(0,3)]
+        public float run_speed_multiplier = 2.0F;
+        public float turn_speed = 2.0F;
+        [Range(-1, 1)]
+        public int y_invert = -1;
+        public bool isDebug = true;
+        public LayerMask object_layer;
 
-	public float move_speed = 10.0F;
-	public float turn_speed = 2.0F;
-	[Range(-1,1)]
-	public int y_invert = -1;
-	public bool isDebug = true;
-	public LayerMask object_layer;
+        private Camera mainCam;
+        private Rigidbody player;
+        private bool isRunning = false;
 
-	private Camera mainCam;
-	private Rigidbody player;
+        void Start()
+        {
 
-	void Start () {
-		
-		player = GetComponent<Rigidbody> ();
-		mainCam = Camera.main;
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	
-	}
-		
-	void Update () {
+            player = GetComponent<Rigidbody>();
+            mainCam = Camera.main;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            player.constraints = RigidbodyConstraints.FreezeRotation;
 
-		if (isDebug) {
+        }
 
-			//Write devolopment build code here
-			if (Input.GetKey (KeyCode.Escape) && !Cursor.visible) {
-				Cursor.visible = true;
-				Cursor.lockState = CursorLockMode.None;
-			} else if (Input.GetMouseButtonDown (0)) {
-				if (Cursor.visible) {
-					Cursor.visible = false;
-					Cursor.lockState = CursorLockMode.Locked;
-				}
-			}
+        void Update()
+        {
 
-		}
+            if (isDebug)
+            {
 
-		#region Movement
-		float rotX = Input.GetAxis ("Mouse Y");
-		float rotY = Input.GetAxis ("Mouse X");
+                //Write devolopment build code here
+                if (Input.GetKey(KeyCode.Escape) && !Cursor.visible)
+                {
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                }
+                else if (Input.GetMouseButtonDown(0))
+                {
+                    if (Cursor.visible)
+                    {
+                        Cursor.visible = false;
+                        Cursor.lockState = CursorLockMode.Locked;
+                    }
+                }
 
-		float moveX = move_speed * Input.GetAxis ("Horizontal");
-		float moveY = move_speed * Input.GetAxis ("Vertical");
+            }
 
-		Vector3 velocity = new Vector3 (0, 0);
-		velocity = transform.forward * moveY + transform.right * moveX;
+            #region Movement
+            float rotX = Input.GetAxis("Mouse Y");
+            float rotY = Input.GetAxis("Mouse X");
 
-		player.velocity = velocity;
+            float moveX = move_speed * Input.GetAxis("Horizontal");
+            float moveY = move_speed * Input.GetAxis("Vertical");
 
-		player.transform.Rotate (Vector3.up * rotY, Space.World);
-		mainCam.transform.Rotate (Vector3.right * rotX * y_invert);
-		#endregion
+            isRunning = Input.GetButton("Sprint") && moveY > 0.0F;
+            if (isRunning) moveY *= run_speed_multiplier;           
 
-		#region Object Detection
-		
-		RaycastHit hit;
-		Ray ray = mainCam.ViewportPointToRay(new Vector2(0.5F,0.5F));
-		if(Physics.Raycast(ray, out hit, 100.0f, object_layer)){
-			GameObject hitObj = hit.collider.gameObject;
-			Debug.Log(hitObj.ToString());
-		}
+            Vector3 velocity = new Vector3(0, 0);
+            velocity = transform.forward * moveY + transform.right * moveX;
 
-		#endregion
-	
-	}
+            player.velocity = velocity;
 
+            player.transform.Rotate(Vector3.up * rotY, Space.World);
+            mainCam.transform.Rotate(Vector3.right * rotX * y_invert);
+            #endregion
+
+            #region Object Detection
+
+            RaycastHit hit;
+            Ray ray = mainCam.ViewportPointToRay(new Vector2(0.5F, 0.5F));
+            if (Physics.Raycast(ray, out hit, 100.0f, object_layer))
+            {
+                GameObject hitObj = hit.collider.gameObject;
+                Debug.Log(hitObj.ToString());
+            }
+
+            #endregion
+
+        }
+
+    }
 }
